00290a815aaedb6ab1b277e182fa989fc2376254
Genysys <samuel.b.dare@gmail.com>
2020-02-10 16:38:31 +0000

    Added python file for sending jmeter transaction

---
+++ hammer/send_jmeter.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# standard library:
+
+
+
+# extend path for imports:
+if __name__ == '__main__' and __package__ is None:
+    from os import sys, path
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+    
+import sys, time, random, json
+from threading import Thread
+from queue import Queue
+from pprint import pprint
+from subprocess import DEVNULL, STDOUT, check_call
+import subprocess
+import os
+
+# pypi:
+import requests # pip3 install requests
+import web3
+from web3 import Web3, HTTPProvider # pip3 install web3
+from web3.utils.abi import filter_by_name, abi_to_signature
+from web3.utils.encoding import pad_hex
+
+# chainhammer:
+from hammer.config import RPCaddress, ROUTE, PRIVATE_FOR, EXAMPLE_ABI
+from hammer.config import PARITY_UNLOCK_EACH_TRANSACTION
+from hammer.config import GAS_FOR_SET_CALL
+from hammer.config import FILE_LAST_EXPERIMENT, EMPTY_BLOCKS_AT_END
+from hammer.clienttools import web3connection, unlockAccount
+
+
+# check_call([cmd, arg1, arg2], stdout=DEVNULL, stderr=STDOUT)
+# block_from = w3.eth.blockNumber
+# rpc =  RPCaddress
+# account = w3.eth.accounts[0]
+
+def send_transactions(rpc , account):
+    with open(os.devnull, 'w') as fp:
+        cmd = subprocess.Popen(("./jmeter.sh", rpc, account), stdout=fp)
+        block_to = w3.eth.blockNumber
+        return block_to
+    
+
+
+def store_experiment_data(success, num_txs, 
+                          block_from, block_to, 
+                          empty_blocks,
+                          filename=FILE_LAST_EXPERIMENT):
+    """
+    most basic data about this last experiment, 
+    stored in same (overwritten) file.
+    Purpose: diagramming should be able to calc proper averages & select ranges
+    """
+    data = {"send" : {
+                "block_first" : block_from,
+                "block_last": block_to,
+                "empty_blocks": empty_blocks, 
+                "num_txs" : num_txs,
+                "sample_txs_successful": success
+                },
+            "node" : {
+                "rpc_address": RPCaddress,
+                "web3.version.node": w3.version.node,
+                "name" : NODENAME,
+                "type" : NODETYPE,
+                "version" : NODEVERSION, 
+                "consensus" : CONSENSUS, 
+                "network_id" : NETWORKID, 
+                "chain_name" : CHAINNAME, 
+                "chain_id" : CHAINID
+                }
+            }
+            
+    with open(filename, "w") as f:
+        json.dump(data, f)
+
+
+if __name__ == '__main__':
+    
+    # check_CLI_or_syntax_info_and_exit()
+
+    global w3, NODENAME, NODETYPE, NODEVERSION, CONSENSUS, NETWORKID, CHAINNAME, CHAINID
+    w3, chainInfos = web3connection(RPCaddress=RPCaddress, account=None)
+    NODENAME, NODETYPE, NODEVERSION, CONSENSUS, NETWORKID, CHAINNAME, CHAINID = chainInfos
+
+
+    block_from = w3.eth.blockNumber
+    rpc =  RPCaddress
+    account = w3.eth.accounts[0]
+    
+    send_transactions(rpc , account)
